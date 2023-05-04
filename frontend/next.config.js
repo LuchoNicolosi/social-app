ef36c74e63d2b20ea08accb8358133d5d2991881
LuchoNicolosi <luchonicolosi01@gmail.com>
2023-05-03 22:57:50 -0300

    add correct env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     JWT_SECRET: 'asdasdasdasdasdasdasdasdasdasdaa',
-    // SERVER_URL: 'https://social-api.up.railway.app',
-    SERVER_URL: 'http://localhost:8080',
+    SERVER_URL: 'https://social-api.up.railway.app',
+    // SERVER_URL: 'http://localhost:8080',
   },
 };
 
